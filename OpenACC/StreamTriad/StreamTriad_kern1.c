d753c12375c9d650d52deddaa29eae46b8d92a11
brobey <brobey@lanl.gov>
2019-10-05 19:12:19 -0600

    Cleaning up build files

---
+++ OpenACC/StreamTriad/StreamTriad_kern1.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "timer.h"
+
+void StreamTriad(double *restrict c, double *restrict a, double *restrict b, double scalar, int nsize);
+
+int main(int argc, char *argv[]){
+
+   int nsize = 20000000, ntimes=16;
+   double *a = malloc(nsize * sizeof(double));
+   double *b = malloc(nsize * sizeof(double));
+   double *c = malloc(nsize * sizeof(double));
+
+   struct timespec tstart;
+   // initializing data and arrays
+   double scalar = 3.0, time_sum = 0.0;
+#pragma acc kernels
+   {
+#pragma acc loop independent
+      for (int i=0; i<nsize; i++) {
+         a[i] = 1.0;
+         b[i] = 2.0;
+      }
+   }
+
+   for (int k=0; k<ntimes; k++){
+      cpu_timer_start(&tstart);
+      // stream triad loop 
+#pragma acc kernels
+      {
+#pragma acc loop independent
+         for (int i=0; i<nsize; i++){
+            c[i] = a[i] + scalar*b[i];
+         }
+      }
+      time_sum += cpu_timer_stop(tstart);
+   }
+
+   printf("Average runtime for stream triad loop is %lf msecs\n", time_sum/ntimes);
+
+   for (int k=0; k<ntimes; k++){
+      cpu_timer_start(&tstart);
+      // stream triad function call
+      StreamTriad(c, a, b, scalar, nsize);
+      time_sum += cpu_timer_stop(tstart);
+   }
+
+   printf("Average runtime for stream triad function call is %lf msecs\n", time_sum/ntimes);
+
+   free(a);
+   free(b);
+   free(c);
+
+   return(0);
+}
+
+void StreamTriad(double *restrict c, double *restrict a, double *restrict b, double scalar, int nsize){
+#pragma acc kernels
+   {
+#pragma acc loop independent
+      for (int i=0; i<nsize; i++){
+         c[i] = a[i] + scalar*b[i];
+      }
+   }
+}
