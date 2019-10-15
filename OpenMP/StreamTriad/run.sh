ee6e4cfd5ae68a3957b38730d48e54f95c42d701
brobey <brobey@lanl.gov>
2019-10-15 13:13:47 -0600

    Adding rough versions of code for OpenMP

---
+++ OpenMP/StreamTriad/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Running StreamTriad"
+./StreamTriad
+echo ""
+echo "Running StreamTriad_par1"
+./StreamTriad_par1
+echo ""
+echo "Running StreamTriad_par2"
+./StreamTriad_par2
+echo ""
+echo "Running StreamTriad_par3"
+./StreamTriad_par3
+
